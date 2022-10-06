623fcad622a61fa0a51e0a1344d45f8e0f0b408c
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-10-06 01:45:13 +0100

    Add header

---
+++ src/app/Header/Header.jsx
@@ -0,0 +1,36 @@
+import SearchIcon from "@mui/icons-material/Search";
+import "./Header.css";
+import HeaderOption from "./HeaderOption/HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationIcon from "@mui/icons-material/Notifications";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png"
+          alt=""
+        />
+
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" name="" id="" />
+        </div>
+      </div>
+      <div className="header__right">
+        <HeaderOption title="Home" Icon={HomeIcon} />
+        <HeaderOption title="My Network" Icon={SupervisorAccountIcon} />
+        <HeaderOption title="Jobs" Icon={BusinessCenterIcon} />
+        <HeaderOption title="Messaging" Icon={ChatIcon} />
+        <HeaderOption title="Notifications" Icon={NotificationIcon} />
+        <HeaderOption title="Abdellah" avatar='https://img.icons8.com/color/48/000000/circled-user-male-skin-type-6.png' />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
