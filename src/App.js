8585435543e37cde35276b16e4dd859b4b8f4067
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-10-06 17:46:38 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import "./app.css";
 import Header from "./app/Header/Header";
+import Sidebar from "./app/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
