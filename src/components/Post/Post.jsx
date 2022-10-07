4ecf671cc8e5bf4f75660e19da75fe3614f06d47
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-10-07 17:43:36 +0100

    Add post components

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -2,9 +2,9 @@ import { Avatar } from "@mui/material";
 import InputOption from "../InputOption/InputOption";
 import "./Post.css";
 import ThumbUpAltOutlinedIcon from "@mui/icons-material/ThumbUpAltOutlined";
-import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
-import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
-import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
 
 const Post = ({ name, description, message, photoUrl }) => {
   return (
@@ -22,10 +22,10 @@ const Post = ({ name, description, message, photoUrl }) => {
       </div>
 
       <div className="post__buttons">
-        <InputOption Icon={ThumbUpAltOutlinedIcon} text="Like" color='gray'/>
-        <InputOption Icon={ChatOutlinedIcon} text="Comment" color='gray'/>
-        <InputOption Icon={ShareOutlinedIcon} text="Share" color='gray'/>
-        <InputOption Icon={SendOutlinedIcon} text="Send" color='gray'/>
+        <InputOption Icon={ThumbUpAltOutlinedIcon} text="Like" color="gray" />
+        <InputOption Icon={ChatOutlinedIcon} text="Comment" color="gray" />
+        <InputOption Icon={ShareOutlinedIcon} text="Share" color="gray" />
+        <InputOption Icon={SendOutlinedIcon} text="Send" color="gray" />
       </div>
     </div>
   );
