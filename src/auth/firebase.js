e745b7970bf75b88e84dd0b7772da8ceb9003e43
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-10-08 12:21:27 +0100

    Revert "Section/widgets"

---
+++ src/auth/firebase.js
@@ -0,0 +1,21 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCMb-1yyKENwsv_2lnhjkbxadox0dmooIA",
+//   authDomain: "linkedin-a4c20.firebaseapp.com",
+//   projectId: "linkedin-a4c20",
+//   storageBucket: "linkedin-a4c20.appspot.com",
+//   messagingSenderId: "666055604355",
+//   appId: "1:666055604355:web:69fb19dcecff76370a03b6",
+//   measurementId: "G-270YLF48Q5"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
